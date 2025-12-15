ba1acff8e3e2cdffc46133d63012704962c3859f
Namdinh13 <abct53085@gmail.com>
2025-12-15 14:21:36 +0700

    Initial commit

---
+++ Assets/TheArtOfWar/Scripts/FPSController/CharController_Motor.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float mouseSensitivity = 2f;
+    public float gravity = -9f;
+    public float jumpHeight = 1.5f;
+    public float waterHeight = 15.5f;
+
+    private CharacterController controller;
+    private Transform playerCamera;
+    private float xRotation = 0f;
+
+    private Vector3 velocity;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        playerCamera = GetComponentInChildren<Camera>().transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Move();
+        Look();
+        CheckWaterHeight();
+        Jump();
+        ApplyGravity();
+    }
+
+    void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal");  // A, D
+        float moveZ = Input.GetAxis("Vertical");    // W, S
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+
+    void Look()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
+    // water check
+    void CheckWaterHeight()
+    {
+        if (transform.position.y < waterHeight)
+        {
+            if (velocity.y < 0)
+                velocity.y = 0;
+        }
+    }
+
+    // jump
+    void Jump()
+    {
+        if (controller.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+    void ApplyGravity()
+    {
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (transform.position.y >= waterHeight)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
